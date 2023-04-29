f14668d5d76415a3a086fe01ee4a911ce832c64a
Vilson1984 <vcmorion@hotmail.com>
2023-04-29 08:42:07 -0300

    Quantidade de jogadas

--- Teimosinha_mega-sena.rb
+++ Teimosinha_mega-sena.rb
@@ -1,33 +1,31 @@
 numero_de_apostas = 0
-aposta_ganhadora = nil 
-
-loop do
-numero_de_apostas += 1 
+aposta_ganhadora = nil
 
 aposta = Array.new 
-
 p "Faça sua aposta"
-4.times do num = gets.to_i
-    aposta << num
+3.times do num = gets.to_i
+  aposta << num
 end
+aposta
+
 p "O seu bilhete é #{aposta}"
 
+loop do
 
-n_disponivel = (1..6).to_a
-n_sorteado = n_disponivel.sample(4)
-
-
-until aposta == aposta_ganhadora do
+  until aposta == aposta_ganhadora
+    numero_de_apostas += 1 
     n_disponivel = (1..6).to_a
-    aposta_ganhadora = n_disponivel.sample(4)
+    aposta_ganhadora = n_disponivel.sample(3)
+    p "Os números sorteados foram #{aposta_ganhadora}"  
+  end
+
+  if aposta.sort == aposta_ganhadora.sort
+    p "O bilhete premiado foi #{aposta_ganhadora}"
+    break
+  else
+    p "vc perdeu" 
+    p "O bilhete premiado foi #{aposta_ganhadora}"
+  end
 end
 
-if aposta == n_sorteado
-    p "O bilhete premiado foi #{n_sorteado}"
-    p "Vc teria sido sorteado na #{numero_de_apostas}º vez"
-    break
-else
-    p "vc perdeu" 
-    p "O bilhete premiado foi #{n_sorteado}"
-end
-end
+p "Vc teria sido sorteado na #{numero_de_apostas}º vez"
