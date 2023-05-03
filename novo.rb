079f2fa325a3294b48bfbf2c686104975221825b
Vilson1984 <vcmorion@hotmail.com>
2023-05-03 12:22:59 -0300

    Uso de classes e funções

--- novo.rb
+++ novo.rb
@@ -1,50 +1,58 @@
 class MegaTeimosinha
-   def f_aposta   
-     @aposta = []
- 
-     p "Faça seu jogo"
-     3.times do 
-       num = gets.to_i
-       @aposta << num
-     end
-     
-     return @aposta.sort
-   end   
+  puts "Olá jogador!"
 
-    def f_sorteio
-      @bilhete_premiado = []
+  puts "Considere a sua APOSTA 3 números distintos entre si, dentre 1 a 10."
+  puts "O SORTEIO também será de 3 números distintos entre si, dentre 1 a 10."
+  puts "Por favor, faça a sua APOSTA!"
 
-      3.times do
-        num_disponiveis = (1..6).to_a
-        @bilhete_premiado << num_disponiveis.sample
-      end
+  def initialize
+    @contador = 0
+  end
 
-      return @bilhete_premiado.sort
-    
-    
+  def f_aposta   
+    @aposta = []
+
+
+    3.times do 
+      num = gets.to_i
+      @aposta << num
     end
-    
-    def compara_jogos
-      loop do
-        until @aposta == @bilhete_premiado
-          f_sorteio
-        end        
+    p "Sua aposta é #{@aposta.sort}"
+  end
 
-        if @aposta == @bilhete_premiado
-          p "Parabéns o seu bilhete #{@bilhete_premiado} foi sorteado"
+  def f_sorteio 
+    
+    @bilhete_premiado = []
+
+    3.times do
+      num_disponiveis = (1..10).to_a
+      @bilhete_premiado = num_disponiveis.sort.sample(3)
+    end
+    p "Bilhete premiado é #{@bilhete_premiado.sort}"
+
+  end
+
+  def f_compara_jogo
+    
+    if @aposta != @bilhete_premiado
+      p "vc perdeu"
+      
+      loop do
+        @contador += 1 
+        f_sorteio
+
+        if @aposta.sort == @bilhete_premiado.sort
+          puts "O bilhete premiado foi #{@bilhete_premiado.sort}."
+          puts "Vc teria acertado na #{@contador}ª jogada."
+          puts "Considerando que o valor do bilhete é R$5,00, vc teria gasto #{@contador * 5},00 reais."
           break
-        else
-          
         end
       end
-      
     end
   end
+end
 
     
 jogo = MegaTeimosinha.new
-#aposta = jogo.f_aposta
-#p "Sua aposta foi #{aposta.sort}"
-#bilhete_premiado = jogo.f_sorteio
-#p "Os números premiados foram #{bilhete_premiado}"
-compara = jogo.compara_jogos
+aposta = jogo.f_aposta
+compara_jogo = jogo.f_compara_jogo
